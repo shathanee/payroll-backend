f2350f4561d7cfc28c407d68853d59e1a71f7bb1
shathanee <shathanee2002@gmail.com>
2025-10-24 09:45:50 +0530

    secondcommit changes in db util file

--- src/main/java/Db/DBUtil.java
+++ src/main/java/Db/DBUtil.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/payroll";
     private static final String USER = "";
+    System.out.println(URL);
     private static final String PASSWORD = "";
 
     static {
